0454aaed05227d0f633610fcdf5108e129e8e47d
Hendrik Tervooren <hendrik@quintly.com>
2020-03-21 19:59:13 +0100

    added static server, first implementation of StoreSelector.js

--- frontend/src/src/forms/BookingForm.js
+++ frontend/src/src/forms/BookingForm.js
@@ -1,22 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import { Form } from 'react-bootstrap';
 import StoreSelector from "./controls/StoreSelector";
 import DateSelector from "./controls/DateSelector";
 
 const Booking = (props) => {
 
-    return (
-        <Form>
-            <Form.Group controlId="bookingForm.store">
-                <Form.Label>Teilnehmende Märkte</Form.Label>
-                <StoreSelector />
-            </Form.Group>
-            <Form.Group controlId="bookingForm.store">
-                <Form.Label>Tag auswählen</Form.Label>
-                <DateSelector />
-            </Form.Group>
-        </Form>
-    );
+        const [selectedStore, setSelectedStore] = React.useState(null)
+        return (<Form>
+                <StoreSelector onSelect={setSelectedStore} />
+            {
+
+                selectedStore &&
+                <Form.Group controlId="bookingForm.store">
+                    <Form.Label>Tag auswählen</Form.Label>
+                    <DateSelector/>
+                </Form.Group>
+            }
+        </Form>)
 }
 
 export default Booking;
