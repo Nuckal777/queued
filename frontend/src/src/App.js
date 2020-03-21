7f85888bddccb7421c0a8f2922e9e10f3d0bc6b4
Hendrik Tervooren <hendrik@quintly.com>
2020-03-21 15:50:05 +0100

    Added react bootstrap

--- frontend/src/src/App.js
+++ frontend/src/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import Overview from "./views/Overview";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
--- frontend/src/src/views/Overview.js
+++ frontend/src/src/views/Overview.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import styles from './Overview.module.css';
 import PropTypes from 'prop-types';
+import { Alert } from 'react-bootstrap';
 
 const Overview = (props) => {
 
     const { data } = props;
 
     return (
-        <div className={styles.wrapper}>
-            {
-                data.map((entry) => <div>{entry.id}</div>)
-            }
-        </div>
+        <>
+            <Alert key={"idx"} variant="primary" >
+                This is a  alert—check it out!
+            </Alert>
+            <div className={styles.wrapper}>
+                {
+                    data.map((entry) => <div>{entry.id}</div>)
+                }
+            </div>
+            </>
     );
 }
 
