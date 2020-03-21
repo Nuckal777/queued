a858c38b4febff6c17eebda248a5dcb6d599c3f3
Tobias Kannenberg <tkannenberg@o2mail.de>
2020-03-21 22:01:03 +0100

    remove constructors from model classes

--- backend/generate_test_data.py
+++ backend/generate_test_data.py
@@ -5,15 +5,26 @@ from models import *
 
 drop_all()
 init_db()
-store1 = Store('Aldi', 'Supermarkt', 'Klopapier erst wieder ab Montag!!!')
-store2 = Store('Netto', 'Supermarkt', 'Alles OK')
-store3 = Store('HackBack', 'Bäcker', None)
+store1 = Store()
+store1.name = 'Aldi'
+store1.type = 'Supermarkt'
+store1.status = 'Klopapier erst wieder ab Montag!!!'
+store2 = Store()
+store2.name = 'Netto'
+store2.type = 'Supermarkt'
+store2.status = 'Alles OK'
+store3 = Store()
+store3.name = 'HackBack'
+store3.type = 'Bäcker'
 db_session.add_all([store1, store2, store3])
 
 user = User()
 db_session.add(user)
 
-booking = Booking(12334, user, store1)
+booking = Booking()
+booking.start_date = 12334
+booking.user = user
+booking.store = store1
 db_session.add(booking)
 
 db_session.commit()
--- backend/models.py
+++ backend/models.py
@@ -9,11 +9,6 @@ class Store(Base):
     store_type = Column(String(50))
     status = Column(String(1000))
 
-    def __init__(self, name=None, store_type=None, status=None):
-        self.name = name
-        self.store_type = store_type
-        self.status = status
-
     def __repr__(self):
         return '<Store {}: {}>'.format(self.id, self.name)
 
@@ -26,20 +21,12 @@ class Booking(Base):
     store_id = Column(Integer, ForeignKey('Store.id'))
     store = relationship('Store')
 
-    def __init__(self, start_date=None, user=None, store=None):
-        self.start_date = start_date
-        self.user = user
-        self.store = store
-
     def __repr__(self):
         return '<Booking {}: {}>'.format(self.id, self.start_date)
 
 class User(Base):
     __tablename__ = 'User'
     id = Column(Integer, primary_key=True)
-
-    def __init__(self):
-        pass
 
     def __repr__(self):
         return '<User {}>'.format(self.id)
