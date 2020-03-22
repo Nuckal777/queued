cf57b63d3692eba1c390487236533e4186944ffc
Tobias Kannenberg <tkannenberg@o2mail.de>
2020-03-22 13:13:39 +0100

    remove ssl, auth, etc

--- backend/app.py
+++ backend/app.py
@@ -1,54 +1,15 @@
 #!/usr/bin/python
 
 from flask import Flask, request, jsonify
-import os, sys
-import logging
-import configparser
-from werkzeug import serving
-from flask_httpauth import HTTPBasicAuth
-import ssl
-#import requests
 from database import init_db
 from database import db_session
 from models import *
 from datetime import datetime
 
 app = Flask(__name__)
-# handle tls with proper webserver
 app.config['JSON_AS_ASCII'] = False
-app.secret_key = os.urandom(24)
-auth = HTTPBasicAuth()
-
-
-loglevel = os.getenv("LOGLEVEL", "DEBUG")
-logger = logging.getLogger("app")
-print("Setting loglevel to: {}".format(loglevel))
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt="%Y-%m-%d %H:%M:%S",
-                    level=loglevel)
-
-config = configparser.ConfigParser()
-if not config.read("settings.ini"):
-    logger.info("config-file: could not read settings.ini")
-    sys.exit(-1)
-
-admin_pw = config["credentials"]["admin_pw"]
-logger.debug("admin_pw is set.")
-
-
-USER_DATA = {
-    "admin": "",
-}
-USER_DATA["admin"] = "{}".format(admin_pw)
-
-logger.info("USER_DATA: %s" % USER_DATA)
 
 init_db()
-
-@auth.verify_password
-def verify(username, password):
-    if not (username and password):
-        return False
-    return USER_DATA.get(username) == password
 
 @app.route('/')
 def index():
@@ -148,13 +109,6 @@ def capacity():
         })
 
     return {"capacity": result_dicts}
-
-# handle tls with proper webserver
-##context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-##context.load_cert_chain("cert-chain.pem", "key.pem")
-##serving.run_simple(host='0.0.0.0', port='5000', ssl_context=context)
-
-#app.run(host='0.0.0.0', port='5000', ssl_context=('cert.pem', 'key.pem'))
 
 app.run(host='0.0.0.0', port=5000)
 
