4d2ea71c4559ad4c27f2a9b3916c74aeccadfc6a
schonaj99 <schonaj99@gmail.com>
2020-03-22 15:18:22 +0100

    Added logo above header

--- frontend/src/src/App.js
+++ frontend/src/src/App.js
@@ -4,13 +4,15 @@ import Overview from "./views/Overview";
 import Header from './layout/Header';
 import Booking from "./views/Booking";
 import Content from "./layout/Content";
+import logo from './images/logo_queued.png';
 
+console.log(logo); //
 
 const getContent = (route) => {
     switch (route) {
         default:
         case 'overview':
-            return <Overview data={[{purchaseID: '1337', startDate: 1584804113}, {purchaseID: '26781', startDate: 1585894113}, {purchaseID: '29365', startDate: 15848136113}]} />
+            return <Overview data={[{purchaseID: '1337', startDate: 1584804113000}, {purchaseID: '26781', startDate: 1585894113000}, {purchaseID: '29365', startDate: 15848136113000}]} />
         case 'booking':
             return <Booking />
     }
@@ -21,6 +23,7 @@ function App() {
 
     return (
     <div className={styles.container}>
+        <img src={logo} alt="Logo" className={styles.header} />
         <Header onClick={setRoute} activeRoute={activeRoute} />
         <Content>
             {
